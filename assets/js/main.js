0fc9a5b00f2e3ffe1fa2c9ee5599bad3341af9e0
Bu <dangnhatnguyen@outlook.com>
2021-05-18 20:48:36 +0700

    asd

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,18 +6,95 @@ function toggleBacktotop() {
   }
 }
 
+function changeNavbarlinksActiveScroll() {
+  let currentPathname = window.location.pathname;
+  var rootHash;
+  var hasActive = false;
+  $("#navbar .homescrollto").each(function (index) {
+    let navbarlink = $(this).get();
+    if (navbarlink.pathname == currentPathname) {
+      if (navbarlink.hash != null && navbarlink.hash != "") {
+        let section = $(hash);
+        if (section.length > 0) {
+          if (position >= section.offset().top && position <= (section.offset().top + section.outerHeight())) {
+            hasActive = true;
+            $(this).addClass('active');
+          } else {
+            $(this).removeClass('active');
+          }
+        } else {
+          $(this).removeClass('active');
+        }
+      } else {
+        rootHash = this;
+      }
+    } 
+  });
+  if(rootHash != null) {
+    if(hasActive) {
+      $(rootHash).removeClass('active');
+    } else {
+      $(rootHash).addClass('active');
+    }
+  }
+}
 
-$(document).ready(function() {
+function scrolltoSection(sectionId) {
+  let offset = $("#header").outerHeight();
+  let section = $(sectionId);
+  if (section.length > 0) {
+      let elementPos = section.offset().top;
+      window.scrollTo({
+          top: elementPos - offset,
+          behavior: 'smooth'
+      });
+  } else {
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+      });
+  }
+}
+
+$(document).ready(function () {
   toggleBacktotop();
-  
-  $('.mobile-nav-toggle').click(function() {
+
+  $('.mobile-nav-toggle').click(function () {
     $('#navbar').toggleClass("navbar-mobile");
     $(this).toggleClass("bi-list");
     $(this).toggleClass("bi-x");
   });
+
+  let currentPathname = window.location.pathname;
+  $("#navbar").each(function (index) {
+    let pathname = $(this).get().pathname;
+    if (pathname == currentPathname) {
+      if ($(this).hasClass(".homescrollto")) {
+        changeNavbarlinksActiveScroll();
+        $(document).scroll(function () {
+          changeNavbarlinksActiveScroll();
+        });
+
+        if (window.location.hash != null && window.location.hash != "") {
+          scrolltoSection(window.location.hash);
+        }
+        $('.homescrollto').click(function (event) {
+            event.preventDefault();
+            if ($('#navbar').hasClass('navbar-mobile')) {
+                $('#navbar').removeClass('navbar-mobile')
+                $('.mobile-nav-toggle').toggleClass('bi-list')
+                $('.mobile-nav-toggle').toggleClass('bi-x')
+            }
+            scrolltoSection($(this).prop("hash"));
+        });
+      } else {
+        $(this).addClass('active');
+      }
+    }
+  });
 });
 
-$(document).scroll(function() {
+$(document).scroll(function () {
   toggleBacktotop();
 });
 
