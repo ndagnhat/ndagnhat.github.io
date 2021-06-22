9a9970ebfd24b5c21a31a2f39a6c06628e4055aa
Sunday <65029229+ngdangnhat@users.noreply.github.com>
2021-06-22 14:37:23 +0700

    sadf

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,24 +6,8 @@ function toggleBacktotop() {
   }
 }
 
-function setActiveNavbar() {
-  {% if page.menu %}
-  let currentMenu = "/{{page.menu}}";
-  {% else %}
-  let currentMenu = "/";
-  {% endif %}
-  let currentPathname = window.location.pathname;
-  $("#navbar .nav-link").each(function (index) {
-    let pathname = $(this).prop("pathname");
-    if (currentMenu == pathname) {
-      $(this).addClass('active');
-    }
-  });
-}
-
 $(document).ready(function () {
   toggleBacktotop();
-  setActiveNavbar();
 
   $('.mobile-nav-toggle').click(function () {
     $('#navbar').toggleClass("navbar-mobile");
